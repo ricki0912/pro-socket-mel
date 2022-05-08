510841ae3016d37f60b9d1db4791f7e2ab3dc504
P <ricki.0912@gmail.com>
2022-05-08 05:28:02 -0500

    version actualizada al 8/05/2022

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
+  const configService = new ConfigService();
+
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  var whitelist = [configService.get('SOCKET_SERVER_ORIGIN')];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
+  //app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
